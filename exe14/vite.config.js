a5f3fc7006bfeb3bc60da9b0caaeb59bf8805a6e
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2026-06-03 21:59:41 -0300

    portifolio

--- exe14/vite.config.js
+++ exe14/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // ou @vitejs/plugin-react
+import react from '@vitejs/plugin-react' // ou @vitejs/plugin-react
 
 // https://vitejs.dev/config/
 export default defineConfig({
